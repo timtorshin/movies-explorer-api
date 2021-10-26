4208604bec0488c54e04e2f34ab8e9f1b4c45624
Timofei Torshin <timtorshin@gmail.com>
2021-10-26 23:17:37 +0300

    refactor: changed database name

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/bitfilmsdb');
+mongoose.connect('mongodb://localhost:27017/moviesdb');
 
 app.use(helmet());
 app.use(limiter);
