24d8cc2b5c02216079b0250ad245fa5aa11262ff
Timofei Torshin <timtorshin@gmail.com>
2021-11-30 20:30:55 +0300

    refactor: tested cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
+const cors = require('cors');
 const { errors } = require('celebrate');
 const authRouther = require('./routes/auth');
 const userRouter = require('./routes/users');
@@ -11,7 +12,7 @@ const auth = require('./middlewares/auth');
 const error = require('./middlewares/error');
 const limiter = require('./middlewares/limiter');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const NotFoundError = require('./utils/NotFoundError');
+// const NotFoundError = require('./utils/NotFoundError');
 
 const { PORT = 3000 } = process.env;
 
@@ -25,7 +26,9 @@ app.use(limiter);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(require('./middlewares/cors'));
+// app.use(require('./middlewares/cors'));
+app.use(cors());
+app.options('*', cors());
 
 app.use(requestLogger);
 
@@ -36,9 +39,9 @@ app.use(auth);
 app.use('/users', userRouter);
 app.use('/movies', movieRouter);
 
-app.use('*', () => {
-  throw new NotFoundError('Запрашиваемый ресурс не найден');
-});
+// app.use('*', () => {
+//   throw new NotFoundError('Запрашиваемый ресурс не найден');
+// });
 
 app.use(errorLogger);
 
