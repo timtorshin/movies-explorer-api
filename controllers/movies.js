d7a47cb3c49c566f0dc7825674563142a62ec378
Timofei Torshin <timtorshin@gmail.com>
2021-11-30 19:48:08 +0300

    refactor: fixed getting movies

--- controllers/movies.js
+++ controllers/movies.js
@@ -4,7 +4,9 @@ const ForbiddenError = require('../utils/ForbiddenError');
 const NotFoundError = require('../utils/NotFoundError');
 
 module.exports.getMovies = (req, res, next) => {
-  Movie.find({})
+  const owner = req.user._id;
+
+  Movie.find({ owner })
     .then((movies) => {
       res.status(200).send(movies);
     })
